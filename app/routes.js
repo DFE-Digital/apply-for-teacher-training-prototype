67d38b02fc875eccf9d145cdd3d1d483bcb4be71
Paul Robert Lloyd <me+git@paulrobertlloyd.com>
2019-06-05 15:15:52 +0100

    Personal details updates

--- app/routes.js
+++ app/routes.js
@@ -1,22 +1,11 @@
 const express = require('express')
 const router = express.Router()
 
+// Application: Personal details
 router.post('/application/personal-details/answer', (req, res) => {
   let nationality = req.session.data['nationality']
 
-  let eea = ['country:AT', 'country:BE', 'country:BG', 'country:HR', 'country:CY', 'country:CZ', 'country:DK', 'country:EE', 'country:FI', 'country:FR', 'country:DE', 'country:GR', 'country:HU', 'country:IS', 'country:IE', 'country:IT', 'country:LV', 'country:LT', 'country:LI', 'country:LU', 'country:MT', 'country:NL', 'country:NO', 'country:PL', 'country:PT', 'country:RO', 'country:SK', 'country:SI', 'country:ES', 'country:SE', 'country:CH', 'country:GB']
-
-  if (eea.includes(nationality)) {
-    res.redirect('/application')
-  } else {
-    res.redirect('/application/personal-details/residency-status')
-  }
-})
-
-router.post('/application/personal-details/answer', (req, res) => {
-  let nationality = req.session.data['nationality']
-
-  let eea = ['country:AT', 'country:BE', 'country:BG', 'country:HR', 'country:CY', 'country:CZ', 'country:DK', 'country:EE', 'country:FI', 'country:FR', 'country:DE', 'country:GR', 'country:HU', 'country:IS', 'country:IE', 'country:IT', 'country:LV', 'country:LT', 'country:LI', 'country:LU', 'country:MT', 'country:NL', 'country:NO', 'country:PL', 'country:PT', 'country:RO', 'country:SK', 'country:SI', 'country:ES', 'country:SE', 'country:CH', 'country:GB']
+  let eea = ['Austrian', 'Belgian', 'Bulgarian', 'Croatian', 'Cypriot', 'Czech', 'Danish', 'Dutch', 'Estonian', 'Finnish', 'French', 'German', 'Greek', 'Hungarian', 'Icelandic', 'Irish', 'Italian', 'Latvian', 'Liechtenstein citizen', 'Lithuanian', 'Luxembourger', 'Maltese', 'Norwegian', 'Polish', 'Portuguese', 'Romanian', 'Slovak', 'Slovenian', 'Spanish', 'Swedish', 'Swiss', 'British']
 
   if (eea.includes(nationality)) {
     res.redirect('/application')

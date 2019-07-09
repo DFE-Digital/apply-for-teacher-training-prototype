df2168a238f0db07ad688eff218e3d2a71ccff07
Paul Robert Lloyd <me+git@paulrobertlloyd.com>
2019-07-09 16:25:44 +0100

    Split up dashboard, add submitted tag

--- app/data/courses.js
+++ app/data/courses.js
@@ -3,12 +3,14 @@ module.exports = {
     name: 'Art and Design',
     code: 'W1X1',
     qualification: 'PGCE with QTS full time',
+    startDate: '2019-09-01',
     provider: 'G56'
   },
   'W5X1': {
     name: 'Dance',
     code: 'W5X1',
     qualification: 'PGCE with QTS full time',
+    startDate: '2019-09-01',
     provider: 'B72'
   }
 }

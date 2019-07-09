adbdd3c6771ce4450ac360168668a624d03b5a6c
Paul Robert Lloyd <me+git@paulrobertlloyd.com>
2019-07-09 15:07:40 +0100

    Spike courses table

---
+++ app/data/courses.js
@@ -0,0 +1,14 @@
+module.exports = {
+  'W1X1': {
+    name: 'Art and Design',
+    code: 'W1X1',
+    qualification: 'PGCE with QTS full time',
+    provider: 'G56'
+  },
+  'W5X1': {
+    name: 'Dance',
+    code: 'W5X1',
+    qualification: 'PGCE with QTS full time',
+    provider: 'B72'
+  }
+}
--- app/data/providers.js
+++ app/data/providers.js
@@ -1,20 +1,10 @@
 module.exports = {
   'G56': {
     name: 'Goldsmiths, University of London',
-    courses: {
-      'W1X1': {
-        name: 'Art and Design',
-        qualification: 'PGCE with QTS full time'
-      }
-    }
+    code: 'G56'
   },
   'B72': {
     name: 'University of Brighton',
-    courses: {
-      'W5X1': {
-        name: 'Dance',
-        qualification: 'PGCE with QTS full time'
-      }
-    }
+    code: 'B72'
   }
 }

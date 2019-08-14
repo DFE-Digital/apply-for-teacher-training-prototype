322e1ed774a69103733ef7833602eadc6dcfbbf5
Paul Robert Lloyd <me+git@paulrobertlloyd.com>
2019-08-14 16:48:15 +0100

    Move email routes into new file

---
+++ app/routes/email.js
@@ -0,0 +1,20 @@
+/**
+ * Email routes
+ */
+module.exports = router => {
+  // Check email
+  router.get('/account/check-email/:action', (req, res) => {
+    res.render(`account/check-email`, {
+      action: req.params.action
+    })
+  })
+
+  // Confirm address
+  router.get('/email/:page/:action', (req, res) => {
+    const page = req.params.page
+
+    res.render(`email/${page}`, {
+      action: req.params.action
+    })
+  })
+}

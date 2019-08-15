908e4632eb3002e816b7edc4c7d68fe80bc94f11
Paul Robert Lloyd <me+git@paulrobertlloyd.com>
2019-08-15 12:38:40 +0100

    Use id instead of gcseId

--- app/routes/application/gcse.js
+++ app/routes/application/gcse.js
@@ -3,7 +3,7 @@ const utils = require('./../../utils')
 
 function gcseData (req) {
   const applicationData = utils.applicationData(req)
-  return applicationData.gcse[req.params.gcseId]
+  return applicationData.gcse[req.params.id]
 }
 
 function isInternational (req) {
@@ -12,7 +12,7 @@ function isInternational (req) {
 
 function gcsePaths (req) {
   const applicationId = req.params.applicationId
-  const basePath = `/application/${applicationId}/gcse/${req.params.gcseId}`
+  const basePath = `/application/${applicationId}/gcse/${req.params.id}`
   const referrer = req.query.referrer
 
   var paths = [
@@ -30,55 +30,55 @@ function gcsePaths (req) {
  * Application: GCSE or equivalent routes
  */
 module.exports = router => {
-  // Generate new degreeID and redirect to that degree
+  // Generate new ID and redirect to that degree
   router.get('/application/:applicationId/gcse/add', (req, res) => {
-    const gcseId = utils.generateRandomString()
-    res.redirect(`/application/${req.params.applicationId}/gcse/${gcseId}/add?${utils.queryString(req)}`)
+    const id = utils.generateRandomString()
+    res.redirect(`/application/${req.params.applicationId}/gcse/${id}/add?${utils.queryString(req)}`)
   })
 
   // Render GCSE review page
   // Note: Must be defined before next route declaration
-  router.get('/application/:applicationId/gcse/:gcseId/review', (req, res) => {
+  router.get('/application/:applicationId/gcse/:id/review', (req, res) => {
     res.render('application/gcse/review', {
-      gcseId: req.params.gcseId
+      id: req.params.id
     })
   })
 
   // Render first page
-  router.get('/application/:applicationId/gcse/:gcseId', (req, res) => {
-    const gcseId = req.params.gcseId
+  router.get('/application/:applicationId/gcse/:id', (req, res) => {
+    const id = req.params.id
     const referrer = req.query.referrer
     const referrerPath = referrer ? `?referrer=${referrer}` : ''
 
     res.render('application/gcse/index', {
-      formaction: `/application/${req.params.applicationId}/gcse/${gcseId}/answer${referrerPath}`,
-      gcseId,
+      formaction: `/application/${req.params.applicationId}/gcse/${id}/answer${referrerPath}`,
+      id,
       referrer
     })
   })
 
   // GCSE type answer branching
-  router.post('/application/:applicationId/gcse/:gcseId/answer', (req, res) => {
-    const gcseId = req.params.gcseId
+  router.post('/application/:applicationId/gcse/:id/answer', (req, res) => {
+    const id = req.params.id
     const applicationId = req.params.applicationId
     const referrer = req.query.referrer
     const type = gcseData(req)['type']
 
     let path
     if (isInternational(req)) {
-      path = `/application/${applicationId}/gcse/${gcseId}/naric`
+      path = `/application/${applicationId}/gcse/${id}/naric`
     } else if (type === 'I don’t have an equivalent qualification yet') {
       path = referrer || `/application/${applicationId}`
     } else {
-      path = `/application/${applicationId}/gcse/${gcseId}/grade`
+      path = `/application/${applicationId}/gcse/${id}/grade`
     }
 
     res.redirect(`${path}?${utils.queryString(req)}`)
   })
 
   // Render NARIC/grade/year pages
-  router.all('/application/:applicationId/gcse/:gcseId/:template(naric|grade|year)', (req, res) => {
-    const gcseId = req.params.gcseId
+  router.all('/application/:applicationId/gcse/:id/:template(naric|grade|year)', (req, res) => {
+    const id = req.params.id
     const referrer = req.query.referrer
     const template = req.params.template
     const paths = gcsePaths(req)
@@ -86,7 +86,7 @@ module.exports = router => {
     res.render(`application/gcse/${template}`, {
       formaction: paths.next,
       paths,
-      gcseId,
+      id,
       referrer
     })
   })

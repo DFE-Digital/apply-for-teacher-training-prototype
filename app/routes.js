ab05d681c779e7afa8e8553ba385780128c13964
Paul Robert Lloyd <me+git@paulrobertlloyd.com>
2019-08-15 11:22:54 +0100

    Move school experience routes into separate routes file

--- app/routes.js
+++ app/routes.js
@@ -20,6 +20,7 @@ require('./routes/application/personal-details')(router)
 require('./routes/application/contact-details')(router)
 require('./routes/application/subject-knowledge')(router)
 require('./routes/application/work-history')(router)
+require('./routes/application/school-experience')(router)
 require('./routes/application/vocation')(router)
 require('./routes/application/degree')(router)
 require('./routes/application/gcse')(router)
@@ -27,77 +28,8 @@ require('./routes/application/qualifications')(router)
 require('./routes/application/other-qualifications')(router)
 require('./routes/application/references')(router)
 require('./routes/application/interview')(router)
+require('./routes/course')(router)
 require('./routes/email')(router)
-
-/**
-  * Application: Generate ID to add new thing
-  * @param {String} section Section of the application
-  * @param {String} thing Thing to add (i.e. job, role, etc.)
-  */
-router.get('/application/:applicationId/school-experience/add/role', (req, res) => {
-  const id = utils.generateRandomString()
-  const queryString = querystring.stringify(req.query)
-
-  res.redirect(`/application/${req.params.applicationId}/school-experience/role/${id}?${queryString}`)
-})
-
-/**
-  * Application: School experience role
-  * @param {String} id ID
-  */
-router.get('/application/:applicationId/school-experience/role/:id', (req, res) => {
-  const id = req.params.id
-  const type = req.params.type
-  const section = req.params.section
-  const queryString = querystring.stringify(req.query)
-  const referrer = req.query.referrer
-
-  res.render(`application/${section}/${type}`, {
-    referrer,
-    formaction: `/application/${req.params.applicationId}/school-experience/update/role/${id}?${queryString}`,
-    id,
-    start: `${req.query.start}`,
-    end: `${req.query.end}`
-  })
-})
-
-/**
-  * Application: School experience - Update job/gap data
-  * Convert individual date components into ISO 8601 date strings
-  * @param {String} type job || gap
-  * @param {String} id Job/gap ID
-  */
-router.post('/application/:applicationId/school-experience/update/role/:id', (req, res) => {
-  const id = req.params.id
-  const applicationId = req.params.applicationId
-  const applicationData = req.session.data.applications[applicationId]['school-experience'][id]
-
-  // Create ISO 8601 start date
-  const startDay = req.body[`${id}-start-date-day`] || '01'
-  const startMonth = req.body[`${id}-start-date-month`]
-  const startYear = req.body[`${id}-start-date-year`]
-  applicationData['start-date'] = false
-
-  if (startMonth && startYear) {
-    applicationData['start-date'] = `${startYear}-${startMonth}-${startDay}`
-  }
-
-  // Create ISO 8601 end date
-  const endDay = req.body[`${id}-end-date-day`] || '01'
-  const endMonth = req.body[`${id}-end-date-month`]
-  const endYear = req.body[`${id}-end-date-year`]
-  applicationData['end-date'] = false
-
-  if (endMonth && endYear) {
-    applicationData['end-date'] = `${endYear}-${endMonth}-${endDay}`
-  }
-
-  res.redirect(req.query.referrer || `/application/${applicationId}/school-experience/review`)
-})
-
-router.get('/application/:applicationId/school-experience/:view', (req, res) => {
-  res.render(`application/school-experience/${req.params.view}`)
-})
 
 router.all('/application/:applicationId/:view', function (req, res) {
   res.render(
@@ -112,7 +44,5 @@ router.all('/application/:applicationId/:view', function (req, res) {
     }
   )
 })
-
-require('./routes/course')(router)
 
 module.exports = router

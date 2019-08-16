c2ea313563ee71c26aa23bceb81bf80676926566
Paul Robert Lloyd <me+git@paulrobertlloyd.com>
2019-08-16 15:54:39 +0100

    Check your answers for subject knowledge

--- app/routes/application/subject-knowledge.js
+++ app/routes/application/subject-knowledge.js
@@ -2,10 +2,11 @@
  * Application: Subject knowledge routes
  */
 module.exports = router => {
-  router.get('/application/:applicationId/subject-knowledge', (req, res) => {
+  router.get('/application/:applicationId/subject-knowledge/:view', (req, res) => {
     const referrer = req.query.referrer
+    const view = req.params.view
 
-    res.render('application/subject-knowledge/index', {
+    res.render(`application/subject-knowledge/${view}`, {
       formaction: referrer || `/application/${req.params.applicationId}`,
       referrer
     })

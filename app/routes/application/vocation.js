acf5cd6ad94c5dd0ffaad53f003e47a69eb29b75
Paul Robert Lloyd <me+git@paulrobertlloyd.com>
2019-08-16 15:59:37 +0100

    Check your answers for vocation

--- app/routes/application/vocation.js
+++ app/routes/application/vocation.js
@@ -2,10 +2,11 @@
  * Application: Vocation routes
  */
 module.exports = router => {
-  router.get('/application/:applicationId/vocation', (req, res) => {
+  router.get('/application/:applicationId/vocation/:view', (req, res) => {
     const referrer = req.query.referrer
+    const view = req.params.view
 
-    res.render('application/vocation/index', {
+    res.render(`application/vocation/${view}`, {
       formaction: referrer || `/application/${req.params.applicationId}`,
       referrer
     })

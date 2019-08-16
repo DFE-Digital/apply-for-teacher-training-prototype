0e2552f587a5913145a540fe9abe614c58a263e1
Paul Robert Lloyd <me+git@paulrobertlloyd.com>
2019-08-16 15:45:21 +0100

    Check your answers for interview preference details

--- app/routes/application/interview.js
+++ app/routes/application/interview.js
@@ -2,10 +2,11 @@
  * Application: Interview routes
  */
 module.exports = router => {
-  router.get('/application/:applicationId/interview', (req, res) => {
+  router.get('/application/:applicationId/interview/:view', (req, res) => {
     const referrer = req.query.referrer
+    const view = req.params.view
 
-    res.render('application/interview/index', {
+    res.render(`application/interview/${view}`, {
       formaction: referrer || `/application/${req.params.applicationId}`,
       referrer
     })

f4fb7d7e0e919ab43777bd153a8239887b9ffa7a
Paul Robert Lloyd <me+git@paulrobertlloyd.com>
2019-08-19 12:54:52 +0100

    Fix personal details review flows

--- app/routes/application/personal-details.js
+++ app/routes/application/personal-details.js
@@ -1,28 +1,30 @@
+const utils = require('./../../utils')
+
 /**
  * Application: Personal details routes
  */
 module.exports = router => {
   // First page
   router.get('/application/:applicationId/personal-details', (req, res) => {
-    const referrer = req.query.referrer
-
     res.render('application/personal-details/index', {
-      formaction: referrer || `/application/${req.params.applicationId}/personal-details/answer`,
-      referrer
+      formaction: `/application/${req.params.applicationId}/personal-details/answer?${utils.queryString(req)}`
     })
   })
 
   // Residency status answer branching
   router.post('/application/:applicationId/personal-details/answer', (req, res) => {
     const applicationId = req.params.applicationId
-    const nationality = req.session.data.applications[applicationId]['candidate']['nationality']
+    const applicationData = req.session.data.applications[applicationId]
+    const nationality = applicationData['candidate']['nationality']
 
     const eea = ['Austrian', 'Belgian', 'Bulgarian', 'Croatian', 'Cypriot', 'Czech', 'Danish', 'Dutch', 'Estonian', 'Finnish', 'French', 'German', 'Greek', 'Hungarian', 'Icelandic', 'Irish', 'Italian', 'Latvian', 'Liechtenstein citizen', 'Lithuanian', 'Luxembourger', 'Maltese', 'Norwegian', 'Polish', 'Portuguese', 'Romanian', 'Slovak', 'Slovenian', 'Spanish', 'Swedish', 'Swiss', 'British']
 
     if (eea.includes(nationality)) {
-      res.redirect(`/application/${applicationId}`)
+      // Delete residency status if previously entered
+      delete applicationData['candidate']['residency-status']
+      res.redirect(`/application/${applicationId}/personal-details/review`)
     } else {
-      res.redirect(`/application/${applicationId}/personal-details/residency-status`)
+      res.redirect(`/application/${applicationId}/personal-details/residency-status?${utils.queryString(req)}`)
     }
   })
 

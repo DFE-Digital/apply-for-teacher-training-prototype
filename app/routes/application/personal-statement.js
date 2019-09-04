f4331ac4d869eb68d885f77b7e401162ead53fd5
Paul Hayes <fofr@users.noreply.github.com>
2019-09-04 14:50:16 +0100

    Remove unused index page

--- app/routes/application/personal-statement.js
+++ app/routes/application/personal-statement.js
@@ -1,23 +1,9 @@
 const providers = require('../../data/providers')
 
 /**
- * Application: Subject knowledge routes
+ * Application: Personal statement routes
  */
 module.exports = router => {
-  router.get('/application/:applicationId/personal-statement/:providerCode', (req, res) => {
-    const referrer = req.query.referrer
-    const providerCode = req.params.providerCode
-    const provider = providers[providerCode]
-
-    res.render(`application/personal-statement/index`, {
-      formaction: referrer || `/application/${req.params.applicationId}`,
-      referrer,
-      providerCode,
-      provider
-    })
-  })
-
-  // Render other cover letter pages
   router.get('/application/:applicationId/personal-statement/:providerCode/:view', (req, res) => {
     const referrer = req.query.referrer
     const providerCode = req.params.providerCode

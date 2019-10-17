2c6143d3788def343050d9ae14b486d52f9d450d
Paul Hayes <fofr@users.noreply.github.com>
2019-10-17 16:50:38 +0100

    Add training with a disability section
    First pass at adding a disabilities section. Content is largely lifted from https://getintoteaching.education.gov.uk/train-to-teach-with-a-disability
    
    Field asking for details about disability probably needs some guidance, and if so would live on a subsequent page.

---
+++ app/routes/application/disability.js
@@ -0,0 +1,25 @@
+const utils = require('./../../utils')
+
+/**
+ * Application: Disability routes
+ */
+module.exports = router => {
+  // First page
+  router.get('/application/:applicationId/disability', (req, res) => {
+    const referrer = req.query.referrer
+
+    res.render('application/disability/index', {
+      referrer,
+      formaction: `/application/${req.params.applicationId}/disability/review`
+    })
+  })
+
+  // Render other disability pages
+  router.get('/application/:applicationId/disability/:view', (req, res) => {
+    const referrer = req.query.referrer
+
+    res.render(`application/disability/${req.params.view}`, {
+      referrer
+    })
+  })
+}

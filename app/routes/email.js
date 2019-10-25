eea64a562d830ae4b39581b2828b143b6f8211f1
Paul Hayes <fofr@users.noreply.github.com>
2019-10-25 14:28:28 +0100

    Use Notify to send sign up emails
    Emails

--- app/routes/email.js
+++ app/routes/email.js
@@ -1,3 +1,22 @@
+const NotifyClient = require('notifications-node-client').NotifyClient
+const notify = new NotifyClient(process.env.NOTIFYAPIKEY)
+
+// Emails will only send if the email address has been whitelisted
+const sendEmail = (req, template) => {
+  const email = req.session.data.account.email
+  if (email) {
+    notify.sendEmail(
+      template,
+      email,
+      {
+        personalisation: {
+          url: req.get('origin')
+        }
+      }
+    )
+  }
+}
+
 /**
  * Email routes
  */
@@ -21,5 +40,10 @@ module.exports = router => {
   // Account created
   router.get('/account/account-created', (req, res) => {
     res.render('account/account-created')
+  })
+
+  router.post('/send-email/create-account', (req, res) => {
+    sendEmail(req, 'd19c241f-73f9-45da-a9cc-6613f9b3d2da')
+    res.redirect('/account/check-email/create-account')
   })
 }

f05d906baf1b36aec1a8821c9b00a95ef30ca832
Paul Robert Lloyd <me+git@paulrobertlloyd.com>
2019-11-06 13:38:50 +0000

    Add ability to send decision emails

--- app/routes/email.js
+++ app/routes/email.js
@@ -1,3 +1,4 @@
+const { DateTime } = require('luxon')
 const NotifyClient = require('notifications-node-client').NotifyClient
 const notify = new NotifyClient(process.env.NOTIFYAPIKEY)
 
@@ -26,11 +27,19 @@ const getCourse = (providerCode, courseCode) => {
   return providers[providerCode].courses[courseCode]
 }
 
+const nowPlusDays = (days, format = 'yyyy-LL-dd') => {
+  const date = DateTime.local().plus({ days: days })
+
+  return DateTime.fromISO(date, {
+    locale: 'en-GB'
+  }).toFormat(format)
+}
+
 /**
  * Email routes
  */
 module.exports = router => {
-  // Account created
+  // Send welcome email when account created
   router.get('/account/account-created', (req, res) => {
     if (!req.session.data.welcomeEmailSent) {
       sendEmail(req, '8aa601b6-9985-471e-bd28-5fe0697820e5')
@@ -40,16 +49,19 @@ module.exports = router => {
     res.redirect('/application/start')
   })
 
+  // Send magic link to verify email address when creating an account
   router.post('/send-email/create-account', (req, res) => {
     sendEmail(req, 'd19c241f-73f9-45da-a9cc-6613f9b3d2da')
     res.redirect('/account/check-email/create-account')
   })
 
+  // Send magic link to verify email address when signing in
   router.post('/send-email/sign-in', (req, res) => {
     sendEmail(req, 'c3457068-675e-4ff9-963e-2e7444607bad')
     res.redirect('/account/check-email/sign-in')
   })
 
+  // Send confirmation email when application submitted
   router.post('/send-email/:applicationId/application-submitted', (req, res) => {
     const applicationId = req.params.applicationId
     const application = req.session.data.applications[applicationId]
@@ -70,5 +82,35 @@ module.exports = router => {
       choiceList: choices.join('\n')
     })
     res.redirect(`/application/${applicationId}/confirmation`)
+  })
+
+  // Send rejected application email
+  router.post('/admin/send-email', (req, res) => {
+    const name = req.session.data.name
+    const notifyTemplate = req.session.data['notify-template']
+
+    let outstandingResponsesList
+    if (notifyTemplate === '2a3478f7-e1bb-45a5-ae3c-2acac1448087') {
+      // Offer template
+      outstandingResponsesList = '* (Name of training provider) - (Name of course)\n* (Name of training provider) - (Name of course)'
+    } else {
+      outstandingResponsesList = '* (Name of training provider) - (Name of course)'
+    }
+
+    sendEmail(req, notifyTemplate, {
+      candidateName: name || 'candidate',
+      providerName: '(Name of training provider)',
+      courseName: '(Name of course)',
+      dbdDate: nowPlusDays(12, 'd MMMM yyyy'),
+      outstandingResponsesList,
+
+      // Offer email
+      courseDate: '5 September 2020',
+      conditionsList: '* Fitness to teach check\n* Disclosure and Barring Service check',
+
+      // Rejected email
+      reasonsList: '* Candidate didn’t come to the interview or assessment'
+    })
+    res.redirect('/admin/send-email-success')
   })
 }

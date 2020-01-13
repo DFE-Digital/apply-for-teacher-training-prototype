7a82ca7641136bbd77a6d81b0846c0825d934494
Paul Robert Lloyd <paulrobertlloyd@users.noreply.github.com>
2020-01-13 17:16:50 +0000

    Add second question variant for training with a disability (#317)
    Co-authored-by: emmajhf <57673845+emmajhf@users.noreply.github.com>

--- app/routes/application/reasonable-adjustments.js
+++ app/routes/application/reasonable-adjustments.js
@@ -5,19 +5,23 @@ module.exports = router => {
   // First page
   router.get('/application/:applicationId/reasonable-adjustments', (req, res) => {
     const referrer = req.query.referrer
+    const option = req.query.option || 'a'
 
     res.render('application/reasonable-adjustments/index', {
       referrer,
+      option,
       formaction: `/application/${req.params.applicationId}/reasonable-adjustments/review`
     })
   })
 
   // Render other reasonable adjustment pages
   router.get('/application/:applicationId/reasonable-adjustments/:view', (req, res) => {
-    const referrer = req.query.referrer
+    const { referrer } = req.query
+    const { option } = req.session.data
 
     res.render(`application/reasonable-adjustments/${req.params.view}`, {
-      referrer
+      referrer,
+      option
     })
   })
 }

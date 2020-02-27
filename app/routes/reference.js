fb92336ecb2f37ec5a9f421fb5ec7ded779ccb22
Paul Robert Lloyd <me+git@paulrobertlloyd.com>
2020-02-27 17:05:03 +0000

    Updated reference flow

--- app/routes/reference.js
+++ app/routes/reference.js
@@ -2,7 +2,26 @@
  * Reference routes
  */
 module.exports = router => {
-  router.get('/reference/finish/answer', (req, res) => {
+  router.post('/reference', (req, res) => {
+    res.redirect('/reference/relationship')
+  })
+
+  router.get('/reference/:template', (req, res) => {
+    res.render(`reference/${req.params.template}`, {
+      referrer: req.query.referrer
+    })
+  })
+
+  router.post('/reference/safeguarding/answer', (req, res) => {
+    const { concern } = req.session.data.reference.safeguarding
+    if (concern === 'Yes') {
+      res.redirect('/reference/review')
+    } else {
+      res.redirect('/reference/comments')
+    }
+  })
+
+  router.post('/reference/finish/answer', (req, res) => {
     const { research } = req.session.data.reference
     if (research === 'yes') {
       res.redirect('/reference/finish?research=true')

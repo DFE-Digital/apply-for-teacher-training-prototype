33231b331515c3b9d9b0020fde4dbc2f9901dd39
Paul Robert Lloyd <me+git@paulrobertlloyd.com>
2020-03-16 11:52:51 +0000

    Add another of same qualification type

--- app/routes/application/other-qualifications.js
+++ app/routes/application/other-qualifications.js
@@ -36,20 +36,35 @@ module.exports = router => {
 
   // Render details page
   router.get('/application/:applicationId/other-qualifications/:id/details', (req, res) => {
+    const applicationData = utils.applicationData(req)
     const { id } = req.params
     const { referrer } = req.query
+    const { type } = applicationData['other-qualifications'][id]
 
     res.render('application/other-qualifications/details', {
       id,
-      referrer
+      referrer,
+      type
     })
   })
 
   router.post('/application/:applicationId/other-qualifications/:id/details', (req, res) => {
+    const applicationData = utils.applicationData(req)
     const next = req.session.data.next || 'review'
-    const { applicationId } = req.params
+    const nextId = utils.generateRandomString()
+    const { applicationId, id } = req.params
     const { referrer } = req.query
+    const { year, org, type } = applicationData['other-qualifications'][id]
 
-    res.redirect(referrer || `/application/${applicationId}/other-qualifications/${next}`)
+    if (next === 'add-type') {
+      applicationData['other-qualifications'][nextId] = {
+        year,
+        org,
+        type
+      }
+      res.redirect(referrer || `/application/${applicationId}/other-qualifications/${nextId}/details`)
+    } else {
+      res.redirect(referrer || `/application/${applicationId}/other-qualifications/${next}`)
+    }
   })
 }

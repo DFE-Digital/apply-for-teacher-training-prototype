fcc384802a04506fcfc0ad33a7e7c1bc454ee098
Frankie Roberto <frankie@frankieroberto.com>
2021-02-08 21:09:47 +0000

    Update reasons for rejection for user testing

--- app/routes/application/dashboard.js
+++ app/routes/application/dashboard.js
@@ -70,7 +70,7 @@ module.exports = router => {
           behaviour: {
             didNotReplyToMessages: true
           },
-          quality_of_application: {
+          qualityOfApplication: {
             subject_knowledge: "Understand the purpose of primary education and then learn more about the procedures related to safeguarding."
           },
           qualifications: {
@@ -249,22 +249,17 @@ module.exports = router => {
         }]
         choices.FGHIJ.status = 'Unsuccessful'
         choices.FGHIJ.feedback = {
-          behaviour: {
-            didNotReplyToMessages: true
+          qualityOfApplication: {
+            personalStatement: "Your rationale for wanting to teach was strong but your personal statement did not demonstrate that you understand the rewards and challenges of teaching. There were a number of spelling and grammar errors throughout the application."
           },
+          additionalFeedback: "It would also strengthen your application if you could get more experience of working within a primary school. ",
           interested_in_future_applications: true
         }
         choices.ZYXWV.status = 'Unsuccessful'
         choices.ZYXWV.interview = null
         choices.ZYXWV.feedback = {
-          behaviour: {
-            other: "TBC",
-            whatTheyCouldToDoImprove: "TBC"
-          },
-          quality_of_application: {
-            subject_knowledge: "TBC"
-          },
-          interested_in_future_applications: true
+          additionalFeedback: "Thank you for your application. After careful consideration, we have decided not to select you for interview for a place on this highly competitive programme.  We wish you all the best with the other applications you have made.",
+          interested_in_future_applications: false
         }
         application.endedWithoutSuccess = false
         break
@@ -325,27 +320,22 @@ module.exports = router => {
       case 'ended-without-success':
         choices.ABCDE.status = 'Unsuccessful'
         choices.ABCDE.feedback = {
-          rejected_by_default: true
+          performanceAtInterview: "You did not show sufficient depth within your responses during the Interview. This was most evident when you were asked about what it means to be a primary teacher.<br><br>You demonstrated a lack of critical evaluation within presentation and research questions.<br><br>The detail we are looking for is outlined in our selection criteria, which can be found attached to the interview invitation letter. For future applications, you could improve by ensuring you are able to cover and evidence the selection criteria.",
+          interested_in_future_applications: true
         }
-
         choices.FGHIJ.status = 'Unsuccessful'
         choices.FGHIJ.feedback = {
-          behaviour: {
-            didNotReplyToMessages: true
+          qualityOfApplication: {
+            personalStatement: "Your rationale for wanting to teach was strong but your personal statement did not demonstrate that you understand the rewards and challenges of teaching. There were a number of spelling and grammar errors throughout the application."
           },
+          additionalFeedback: "It would also strengthen your application if you could get more experience of working within a primary school. ",
           interested_in_future_applications: true
         }
         choices.ZYXWV.status = 'Unsuccessful'
         choices.ZYXWV.interview = null
         choices.ZYXWV.feedback = {
-          behaviour: {
-            other: "TBC",
-            whatTheyCouldToDoImprove: "TBC"
-          },
-          quality_of_application: {
-            subject_knowledge: "TBC"
-          },
-          interested_in_future_applications: true
+          additionalFeedback: "Thank you for your application. After careful consideration, we have decided not to select you for interview for a place on this highly competitive programme.  We wish you all the best with the other applications you have made.",
+          interested_in_future_applications: false
         }
         application.endedWithoutSuccess = true
         break

dd44f88e3a05d0f91412dace175d82bac3a8b037
Paul Robert Lloyd <me+git@paulrobertlloyd.com>
2021-08-27 15:45:31 +0100

    Tweak contact information file names and guidance

--- app/routes/application/contact-information.js
+++ app/routes/application/contact-information.js
@@ -2,16 +2,16 @@ const utils = require('../../utils')
 
 module.exports = router => {
   // Render other contact pages
-  router.get('/application/:applicationId/contact-information/where-do-you-live', (req, res) => {
+  router.get('/application/:applicationId/contact-information/contact-address', (req, res) => {
     const { referrer } = req.query
 
-    res.render('application/contact-information/where-do-you-live', {
+    res.render('application/contact-information/contact-address', {
       referrer
     })
   })
 
   // Contact address type answer branching
-  router.all('/application/:applicationId/contact-information/where-do-you-live/answer', (req, res) => {
+  router.all('/application/:applicationId/contact-information/contact-address/answer', (req, res) => {
     const { applicationId } = req.params
     const application = utils.applicationData(req)
     const { addressType } = application.contactInformation
@@ -21,20 +21,20 @@ module.exports = router => {
       if (addressLookup) {
         res.redirect(`/application/${applicationId}/contact-information/lookup-address`)
       } else {
-        res.redirect(`/application/${applicationId}/contact-information/uk-address`)
+        res.redirect(`/application/${applicationId}/contact-information/uk-contact-address`)
       }
     } else {
-      res.redirect(`/application/${applicationId}/contact-information/international-address`)
+      res.redirect(`/application/${applicationId}/contact-information/international-contact-address`)
     }
   })
 
   // Second address type answer branching
-  router.all('/application/:applicationId/contact-information/second-address/answer', (req, res) => {
+  router.all('/application/:applicationId/contact-information/permanent-address/answer', (req, res) => {
     const { applicationId } = req.params
     const application = utils.applicationData(req)
-    const { secondAddress } = application.contactInformation
+    const { permanentAddressType } = application.contactInformation
 
-    if (secondAddress === 'contactAddess') {
+    if (permanentAddressType === 'contact') {
       res.redirect(`/application/${applicationId}/contact-information/review`)
     } else {
       res.redirect(`/application/${applicationId}/contact-information/where-is-your-permanent-address`)
@@ -55,7 +55,7 @@ module.exports = router => {
   })
 
   // Render address page
-  router.get('/application/:applicationId/contact-information/:view(uk-address|international-address)', (req, res) => {
+  router.get('/application/:applicationId/contact-information/:view(uk-contact-address|international-contact-address)', (req, res) => {
     const { referrer } = req.query
     const { view } = req.params
 

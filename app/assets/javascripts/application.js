db3baf7325ee2a1396366e0966f2907a498bdacf
Frankie Roberto <frankie@frankieroberto.com>
2022-10-12 12:34:28 +0100

    Survey users on whether provider rejection feedback is helpful (#727)

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -6,3 +6,27 @@ if (window.console && window.console.info) {
 document.addEventListener('DOMContentLoaded', function () {
   window.GOVUKFrontend.initAll()
 })
+
+function setupFeedbackButtons () {
+  var feedbackButtons = document.querySelectorAll('*[data-feedback]')
+
+  for (feedbackButton of feedbackButtons) {
+    feedbackButton.addEventListener('click', function (event) {
+      var feedbackElement = event.target.parentElement
+
+      var feedback = event.target.getAttribute('data-feedback')
+
+      if (feedback == 'Yes') {
+        feedbackElement.innerHTML = 'You said that this feedback is helpful.'
+      } else {
+        feedbackElement.innerHTML = 'You said that this feedback is not helpful.'
+      }
+
+      event.preventDefault()
+    })
+  }
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+  setupFeedbackButtons()
+})

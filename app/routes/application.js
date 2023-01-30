0fba4f7fbba072930d3199b3a0060c98d2ebb331
Frankie Roberto <frankie@frankieroberto.com>
2023-01-30 17:13:38 +0000

    Update contact info section

--- app/routes/application.js
+++ app/routes/application.js
@@ -164,7 +164,7 @@ module.exports = router => {
 
   // require('./application/choices')(router)
   // require('./application/personal-information')(router)
-  // require('./application/contact-information')(router)
+  require('./application/contact-information')(router)
   // require('./application/english-language')(router)
   // require('./application/work-history')(router)
   // require('./application/unpaid-experience')(router)

dc15f30f08e07946025f89a347ae734ae6cf6645
Frankie Roberto <frankie@frankieroberto.com>
2023-02-15 15:05:42 +0000

    remove default session data

--- app/data/application-single-choice.js
+++
@@ -1,8 +0,0 @@
-// Copies dummy application and tweaks some fields
-const application = JSON.parse(JSON.stringify(require('./application')))
-
-application.choices = {
-  ABCDE: application.choices.ABCDE
-}
-
-module.exports = application
